2bf8cb15d68cad2a684b003e682e864fc559d167
loftyyyy <lofty08.business@gmail.com>
2024-06-09 21:11:53 +0800

    Hi

---
+++ src/main/java/org/example/QuizCardBuilder.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.File;
+
+public class QuizCardBuilder {
+    public void go(){
+
+    }
+
+    public void nextCard(){
+
+    }
+
+    public void saveCard(){
+
+    }
+
+    public void clearCard(){
+
+    }
+
+    private void saveFile(File file){
+
+    }
+}
